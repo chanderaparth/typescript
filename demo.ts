a0024da311c5fb9dc38cf844537a3cc409c92da4
parth <chnderachanderaparth986@gmail.com>
2023-11-02 17:15:40 +0530

    ts

--- demo.ts
+++ demo.ts
@@ -132,3 +132,32 @@
               
 //   printYardSize(home);
 
+// class NamedValue<T> {
+//     private _value: T | undefined;
+  
+//     constructor(private name: string) {}
+  
+//     public setValue(value: T) {
+//       this._value = value;
+//     }
+  
+//     public getValue(): T | undefined {
+//       return this._value;
+//     }
+  
+//     public toString(): string {
+//       return `${this.name}: ${this._value}`;
+//     }
+//   }
+        
+//   const value = new NamedValue<number>('myNumber');
+  
+//   value.setValue(10);
+  
+//   console.log(value.toString());
+
+
+
+
+
+
